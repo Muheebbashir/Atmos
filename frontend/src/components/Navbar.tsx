8e7ad1d824b280ecdaf4f7e6d3b574e4c2f152b3
Muheeb Bashir <your_email@example.com>
2026-01-30 16:39:45 +0530

    feat(Navbar): implement navigation bar with routing and action buttons

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,8 +1,30 @@
+import { useNavigate } from "react-router-dom"
+import {  ChevronRight, ChevronLeft } from "lucide-react"
 
 
 function Navbar() {
+  const navigate=useNavigate();
   return (
-    <div>Navbar</div>
+    <>
+    <div className="w-full flex justify-between items-center font-semibold ">
+      <div className="flex items-center gap-2">
+        <ChevronLeft className="bg-black rounded-2xl cursor-pointer w-8  " onClick={()=>navigate(-1)} />
+          <ChevronRight className="bg-black rounded-2xl cursor-pointer w-8  " onClick={()=>navigate(+1)} />
+
+      </div>
+      <div className="flex items-center gap-4">
+        <p className="px-4 py-1 cursor-pointer bg-white text-black text-[15px] rounded-full hidden md:block">Explore Premium</p>
+        <p className="px-4 py-1 cursor-pointer bg-white text-black text-[15px] rounded-full hidden md:block">Install App</p>
+        <p className="px-4 py-1 cursor-pointer bg-white text-black text-[15px] rounded-full">Logout</p>
+      </div>
+    </div>
+    <div className="flex items-center gap-2 mt-4">
+      <p className="bg-white text-black px-4 py-1 rounded-2xl cursor-pointer">All</p>
+      <p className="bg-white text-black px-4 py-1 rounded-2xl cursor-pointer hidden md:block">Music</p>
+      <p className="bg-white text-black px-4 py-1 rounded-2xl cursor-pointer hidden md:block">Podcasts</p>
+      <p className="bg-white text-black px-4 py-1 rounded-2xl cursor-pointer md:hidden" onClick={()=>navigate("/playlist")}>PlayList</p>
+    </div>
+    </>
   )
 }
 
